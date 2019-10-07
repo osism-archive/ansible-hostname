bb4afd994ea8cd7f3738561e55f4648fdcb85ae0
Christian Berendt <berendt@betacloud-solutions.de>
2019-10-07 22:07:47 +0200

    molecule: add 2 more testinfra tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,3 +2,5 @@ def test_hostname_file(host):
     f = host.file("/etc/hostname")
     assert f.exists
     assert f.is_file
+    assert f.mode == 0o644
+    assert f.contains(host.check_output("hostname -s"))
